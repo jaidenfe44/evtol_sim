1ded232fe7e6aeb268175cad4b5d1c0a210434b9
Jaiden Ferraccioli <jaidenfe@gmail.com>
2025-09-09 23:00:28 -0400

    finished Port::removeFromCharger() function test case

--- tst/TestCase_Port.cpp
+++ tst/TestCase_Port.cpp
@@ -72,8 +72,8 @@ bool TestCase_Port::TC_addToCharger()
 
     // Verify expected behavior
     tcAssert(chargers_a[chargerIdx].chargingQueue.size() == 1, __func__, "Charger queue does not contain a vehicle, queue is empty");
-    tcAssert(chargers_a[chargerIdx].chargingQueue.front() == &vehicles_a[vehicleIdx], __func__, "TODO");
-    tcAssert(chargers_a[chargerIdx].chargingQueue.front()->state == VehicleState::eCharging, __func__, "TODO");
+    tcAssert(chargers_a[chargerIdx].chargingQueue.front() == &vehicles_a[vehicleIdx], __func__, "The vehicle is not at the front of the charger queue");
+    tcAssert(chargers_a[chargerIdx].chargingQueue.front()->state == VehicleState::eCharging, __func__, "The state of the vehicle at the front of the charger queue is not eCharging");
     tcAssert(vehicles_a[vehicleIdx].vehicleCharger == chargerIdx, __func__, "The added vehicle did not update the charger index properly");
     tcAssert(nextAvailableCharger != &chargers_a[chargerIdx], __func__, "Next available charger was not update, still pointing to the same Charger");
 
@@ -127,13 +127,41 @@ bool TestCase_Port::TC_addToCharger()
 
 bool TestCase_Port::TC_removeFromCharger()
 {
-    // TODO: Initialize test case
+    // Initialize test case
     tcPass = true;
+    vehicles_a[0].state = VehicleState::eCharging;
+    vehicles_a[0].vehicleCharger = 1;
+    vehicles_a[1].state = VehicleState::eQueued;
+    vehicles_a[1].vehicleCharger = 1;
+    vehicles_a[2].state = VehicleState::eQueued;
+    vehicles_a[2].vehicleCharger = 1;
 
-    // TODO: Execute Function
 
-    // TODO: Verify expected behavior
-    tcAssert(false, __func__, "TODO");
+    while(!chargers_a[1].chargingQueue.empty()) chargers_a[1].chargingQueue.pop();
+    chargers_a[1].chargingQueue.push(&vehicles_a[0]);
+    chargers_a[1].chargingQueue.push(&vehicles_a[1]);
+    chargers_a[1].chargingQueue.push(&vehicles_a[2]);
+
+    // Verify initial conditions
+    tcAssert(chargers_a[1].chargingQueue.size() == 3, __func__, "Charging queue size is not the expected value (3)");
+    tcAssert(chargers_a[1].chargingQueue.front() == &vehicles_a[0], __func__, "First vehicle added was not at the front of the charger queue");
+
+    // Execute Function
+    removeFromCharger(&vehicles_a[1]);
+
+    // Verify expected behavior
+    tcAssert(chargers_a[1].chargingQueue.size() == 3, __func__, "The charging queue was modified unexpectedly");
+    tcAssert(chargers_a[1].chargingQueue.front() == &vehicles_a[0], __func__, "The front of the charger queue has changed unexpectedly");
+
+    // Execute function
+    removeFromCharger(&vehicles_a[0]);
+
+    // Verify expected behavior
+    tcAssert(chargers_a[1].chargingQueue.size() == 2, __func__, "The charging queue size does not show a vehicle has been removed");
+    tcAssert(chargers_a[1].chargingQueue.front() == &vehicles_a[1], __func__, "The front of the charging queue has not been updated to point to the second vehicle added");
+    tcAssert(vehicles_a[0].state == VehicleState::eAirborne, __func__, "Removed vehicle state was not updated to eAirborne");
+    tcAssert(vehicles_a[1].state == VehicleState::eCharging, __func__, "The state of the second added vehicle has not been updated to eCharging");
+    tcAssert(vehicles_a[2].state == VehicleState::eQueued, __func__, "The state of the third vehicle was modified when it should have remained eQueued");
 
     // Cleanup
     cleanup();
